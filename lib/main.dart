a9a9a7a7fd0ed9edffde3585061820cc58979fb6
Ozan Sahin <ozansahin.ac@gmail.com>
2022-05-25 17:26:04 +0300

    settings buttons state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/my_home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:settings_ui/settings_ui.dart';
 
-class SettingsPage extends StatelessWidget {
+final languageProvider = StateProvider((ref) => 'English');
+final darkModeProvider = StateProvider((ref) => false);
+final measurementProvider = StateProvider((ref) => false);
+
+class SettingsPage extends ConsumerWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -23,7 +28,19 @@ class SettingsPage extends StatelessWidget {
                 title: const Text('Language'),
                 value: const Text('English'),
                 leading: const Icon(Icons.language),
-              )
+              ),
+              SettingsTile.switchTile(
+                initialValue: ref.watch(darkModeProvider),
+                onToggle: (value) => ref.read(darkModeProvider.notifier).state = value,
+                title: const Text('Dark Mode'),
+                leading: const Icon(Icons.brightness_2),
+              ),
+              SettingsTile.switchTile(
+                initialValue: ref.watch(measurementProvider),
+                onToggle: (value) => ref.read(measurementProvider.notifier).state = value,
+                title: const Text('Measurement System'),
+                leading: const Icon(Icons.architecture),
+              ),
             ],
           ),
         ],
