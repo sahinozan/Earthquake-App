7f604c9f724cf6131654368a017b66e9720e422e
Ozan Sahin <ozansahin.ac@gmail.com>
2022-06-25 23:45:50 +0300

    unused functions removed

--- lib/earthquake.dart
+++ lib/earthquake.dart
@@ -10,13 +10,6 @@ class Earthquake with _$Earthquake {
     required List<Feature> features,
   }) = _Earthquake;
 
-  static Earthquake fromSnapshot(snapshot) {
-    return Earthquake(
-      type: '',
-      features: [Feature.fromSnapshot(snapshot)],
-    );
-  }
-
   factory Earthquake.fromJson(Map<String, dynamic> json) =>
       _$EarthquakeFromJson(json);
 }
@@ -31,14 +24,6 @@ class Feature with _$Feature {
 
   factory Feature.fromJson(Map<String, dynamic> json) =>
       _$FeatureFromJson(json);
-
-  static Feature fromSnapshot(snapshot) {
-    return Feature(
-      properties: Properties.fromSnapshot(snapshot),
-      id: snapshot.data()['id'],
-      geometry: Geometry.fromSnapshot(snapshot),
-    );
-  }
 }
 
 @freezed
@@ -51,14 +36,6 @@ class Properties with _$Properties {
 
   factory Properties.fromJson(Map<String, Object?> json) =>
       _$PropertiesFromJson(json);
-
-  static Properties fromSnapshot(snapshot) {
-    return Properties(
-      mag: snapshot.data()['mag'],
-      place: snapshot.data()['place'],
-      time: snapshot.data()['time'],
-    );
-  }
 }
 
 @freezed
@@ -69,14 +46,4 @@ class Geometry with _$Geometry {
 
   factory Geometry.fromJson(Map<String, dynamic> json) =>
       _$GeometryFromJson(json);
-
-  static Geometry fromSnapshot(snapshot) {
-    return Geometry(
-      coordinates: [
-        snapshot.data()['coordinates'][0],
-        snapshot.data()['coordinates'][1],
-        snapshot.data()['coordinates'][2],
-      ],
-    );
-  }
 }
