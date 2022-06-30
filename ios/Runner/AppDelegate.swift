765517787f5759a0e628f234909e87eba4ab953a
Ozan Sahin <ozansahin.ac@gmail.com>
2022-06-30 15:01:57 +0300

    cocoapods issue fixed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,19 +1,23 @@
 import UIKit
 import Flutter
 import GoogleMaps
+// import Firebase
 import FirebaseCore
 import FirebaseFirestore
-import FirebaseAuth
+// import FirebaseAuth
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  override init() {
+    super.init()
+    FirebaseApp.configure()
+  }
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyAIZXaAnpyY-Rzk2LUFEgy_TkzTuOj2k00")
-    FirebaseApp.configure()
+    GMSServices.provideAPIKey("AIzaSyDX3OXYNrfuuKWvsY8Z4sSj7hjudUTW7bg")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
