7cc207f4938c173bbd22fd09ad955193ee853f77
Ozan Şahin <ozansahin.ac@gmail.com>
2022-07-11 12:36:40 +0300

    ios support added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,23 +1,16 @@
 import UIKit
 import Flutter
 import GoogleMaps
-// import Firebase
-import FirebaseCore
-import FirebaseFirestore
-// import FirebaseAuth
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override init() {
-    super.init()
-    FirebaseApp.configure()
-  }
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyCcdR9Aq0go0sqtxO0bDXu3jreK4hhs4BM")
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyDX3OXYNrfuuKWvsY8Z4sSj7hjudUTW7bg")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
